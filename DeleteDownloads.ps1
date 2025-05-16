3fac5f43b784d79d6f098d02f2c2351924c4168f
LinasRadavicius <86026448+LinasRadavicius@users.noreply.github.com>
2025-05-16 08:48:46 +0100

    Add files via upload
    Deletes all files in every users Downloads directory

---
+++ DeleteDownloads.ps1
@@ -0,0 +1,5 @@
+﻿$AllProfiles = Get-ChildItem "C:\Users" | ? {$_.Name -notin @("Public","Default User","Temp","All Users") -and (Test-Path "$($_.FullName)\Downloads")}
+
+foreach ($profile in $AllProfiles) {
+$downloadsPath = "$($profile.FullName)\Downloads"
+Get-ChildItem -Path $downloadsPath -Recurse -Force -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue }
