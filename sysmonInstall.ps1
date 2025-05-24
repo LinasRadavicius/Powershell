6f4aa17271bd8bb332e959119d63143fda7595e7
LinasRadavicius <86026448+LinasRadavicius@users.noreply.github.com>
2025-05-24 15:55:59 +0100

    Sysmon Install
    This script first checks if sysmon is installed if not it will install, If it's already installed it will check the hash of the current config and compare with most up to date config.

---
+++ sysmonInstall.ps1
@@ -0,0 +1,30 @@
+﻿$sysmonExePath = "\\linax-dc\Share\Sysmon\Sysmon64.exe"
+$sysmonConfig = "\\linax-dc\Share\Sysmon\sysmonconfig-export.xml"
+
+
+## in this use it dynamically as below OR you can paste hash manually
+$configHash = (Get-FileHash $sysmonConfig).Hash
+
+## ^^ Defining paths to installs || Check if Sysmon already exists
+$sysmonService = Get-Service "Sysmon64" -ErrorAction SilentlyContinue
+
+if ($null -ne $sysmonService)
+{
+    ##Get current config hash 
+    $configOutput = & $sysmonExePath -c
+    $hashLine = ($configOutput | ? { $_ -match "SHA256=" }) -replace ".*SHA256=", ""
+    Write-Output("Current hash: " + $hashLine )
+
+    if ($hashLine -eq $configHash ){
+        
+        return
+    }
+    else{
+        & $sysmonExePath -c $sysmonConfig
+
+    }
+
+}
+else{
+    & $sysmonExePath -i $sysmonConfig -accepteula 
+}
